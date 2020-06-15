44fa90fd4ab33e92e60abfc2b7fd9df0550bf003
mattt333 <mat.malric@gmail.com>
2020-06-15 10:21:03 +0200

    finito

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,14 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import java.util.List;
 
 @Entity
 public class Course {
@@ -13,7 +18,19 @@ public class Course {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "courses")
+    
+    private List<Wizard> wizards = new ArrayList <>();
+
     public Course() {
+    }
+
+    public List<Wizard> getWizard (){
+        return this.wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards=wizards;
     }
 
     public Course(String name) {
